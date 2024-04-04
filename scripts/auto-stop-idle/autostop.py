4d57f7297dd0d84e011051bfabeeb02e34caf8d1
Alexei Martsinkovskiy <alexei.martsinkovskiy@gmail.com>
2024-04-04 15:36:11 -0700

    update autostop py
    updated to also upload a data folder to the sagemaker bucket. this is so that we can do faster loading upon starting an instance, as sagemaker download speeds are poor.

--- scripts/auto-stop-idle/autostop.py
+++ scripts/auto-stop-idle/autostop.py
@@ -13,6 +13,8 @@
 
 import requests
 from datetime import datetime
+import os
+import sagemaker
 import getopt, sys
 import urllib3
 import boto3
@@ -116,6 +118,17 @@ else:
         print('Notebook idle state set as %s since no sessions detected.' % idle)
 
 if idle:
+    local_dir="/home/ec2-user/SageMaker/data"
+    sess = sagemaker.Session()
+    bucket= sess.default_bucket()
+    client = boto3.client('s3')
+    for root, dirs, files in os.walk(local_directory):
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            relative_path = os.path.relpath(local_path, local_directory)
+            s3_path = os.path.join(destination, relative_path)
+            print("Uploading %s..." % s3_path)
+            client.upload_file(local_path, bucket, s3_path)
     print('Closing idle notebook')
     client = boto3.client('sagemaker')
     client.stop_notebook_instance(
