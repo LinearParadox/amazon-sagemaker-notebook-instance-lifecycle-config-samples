149c43710387404f2c2a78b62d1bcbeadc09834e
Alexei Martsinkovskiy <alexei.martsinkovskiy@gmail.com>
2024-04-23 13:36:03 -0700

    Update autostop.py

--- scripts/auto-stop-idle/autostop.py
+++ scripts/auto-stop-idle/autostop.py
@@ -123,8 +123,9 @@ if idle:
     sess = sagemaker.Session()
     bucket= sess.default_bucket()
     sess.upload_data(local_dir, bucket=None, key_prefix=get_notebook_name(), callback=None, extra_args={"StorageClass":"INTELLIGENT_TIERING"})
+    print("uploaded local data")
     sess.upload_data(env, bucket=None, key_prefix=get_notebook_name(), callback=None, extra_args={"StorageClass":"INTELLIGENT_TIERING"})
-    print('Closing idle notebook')
+    print('Uploaded environment. Closing idle notebook')
     client = boto3.client('sagemaker')
     client.stop_notebook_instance(
         NotebookInstanceName=get_notebook_name()
